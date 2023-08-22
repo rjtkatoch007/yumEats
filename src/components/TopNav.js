8ddd44f3c2532416eb5ed453b3902fd6af8146ef
rajat <rajat.k@sphinxworldbiz.co>
2023-08-22 14:35:04 +0530

    TopNav and Sidebar components finished

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
-import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose, AiFilltag} from 'react-icons/ai'
+import {AiOutlineMenu, AiOutlineSearch, AiOutlineClose} from 'react-icons/ai'
 import {BsFillCartFill, BsPerson} from 'react-icons/bs'
+import {TbTruckReturn} from 'react-icons/tb'
+import {FaGoogleWallet} from 'react-icons/fa'
+import {MdHelp, MdOutlineFavorite} from 'react-icons/md'
 
 const TopNav = () => {
   const [sideNav, setSideNav] = useState(false);
@@ -12,7 +15,7 @@ const TopNav = () => {
                     <AiOutlineMenu size={25}/>                
             </div>
             <h1 className='text-2xl sm:text-3xl lg:text-4xl px-2'>Yum
-                <span>Eats</span>
+                <span className='font-bold'>Eats</span>
             </h1>
             <div className='hidden lg:flex items-center bg-gray-200 rounded-full p-1 text-[14px]'>
                 <p className='bg-orange-700 text-white rounded-full p-2 text-bold'>Free</p>
@@ -28,14 +31,42 @@ const TopNav = () => {
         </button>
         {
           sideNav ? (
-            <div className='bg-black/60 fixed w-full h-screen z-10 top-0 left-0'></div>
+            <div className='bg-black/60 fixed w-full h-screen z-10 top-0 left-0'
+            onClick={()=>setSideNav(!sideNav)}
+            ></div>
           ): ("")
         }
 
-        <div className={sideNav ?
-         'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-200' 
-        : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-200'} >
-
+        <div className={
+          sideNav 
+          ? 'fixed top-0 left-0 w-[300px] h-screen bg-white z-10 duration-300' 
+          : 'fixed top-0 left-[-100%] w-[300px] h-screen bg-white z-10 duration-300'} >
+            <AiOutlineClose className='absolute right-4 top-4 cursor-pointer' onClick={()=>setSideNav(!sideNav)} size={25}/>
+            <h2 className='text-2xl p-4'>Yum <span className='text-orange-700 font-bold'>Eats</span></h2>
+              <nav>
+                <ul className='flex flex-col p-4 text-gray-900'>
+                  <li className='flex text-xl p-4'>
+                  <BsPerson className='mr-4 text-white bg-black rounded-full' size={25}/>
+                  My Account
+                  </li>
+                  <li className='flex text-xl p-4'>
+                  <TbTruckReturn className='mr-4 text-white bg-black rounded-full' size={25}/>
+                  Delivery
+                  </li>
+                  <li className='flex text-xl p-4'>
+                  <MdOutlineFavorite className='mr-4 text-white bg-black rounded-full' size={25}/>
+                  My Favourite
+                  </li>
+                  <li className='flex text-xl p-4'>
+                  <FaGoogleWallet className='mr-4 text-white bg-black rounded-full' size={25}/>
+                  My Wallet
+                  </li>
+                  <li className='flex text-xl p-4'>
+                  <MdHelp className='mr-4 text-white bg-black rounded-full' size={25}/>
+                  Help
+                  </li>
+                </ul>
+              </nav>
         </div>
     </div>
   )
