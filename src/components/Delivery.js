27d1d13f578e3e197cd4df73a2f26aec360f4491
rajat <rajat.k@sphinxworldbiz.co>
2023-08-30 18:39:52 +0530

    quick delivery component finished

---
+++ src/components/Delivery.js
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const Delivery = () => {
+  return (
+    <div className='w-full bg-white py-16 px-4'>
+        <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Delivery App</h3>
+        <div className='w-[1240px] mx-auto grid md:grid-cols-2'>
+            <img className='w-[550px] mx-auto my-4' src="https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693398972/home-foodpanda-apps_nspunp.webp" alt='pic'/>
+            <div className='flex flex-col justify-center'>
+                <p className='text-[#00df9a] font-bold text-center'>Get The App</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience on-demand</h1>
+                <p>
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vel tempora hic aliquam dolorum nisi similique non, officia cumque, saepe quod sint labore aut, placeat ratione nam id! Mollitia, aut voluptatum.
+                    Laborum earum quasi animi minus expedita ipsa, odit libero eligendi repellendus, nisi in quo possimus odio iusto illum sit necessitatibus doloremque hic incidunt mollitia aperiam molestias quos. Error, porro alias.
+                    Iusto sed voluptatibus vitae explicabo, quaerat deserunt expedita optio sunt assumenda impedit maxime maiores voluptates cum corrupti cupiditate labore harum nam nesciunt libero perspiciatis at itaque dolorem dolorum? Quas, tempora?
+                    Ratione quo eos explicabo ex sint eligendi, aspernatur repellat natus sit molestias exercitationem laboriosam, modi asperiores provident. Obcaecati ratione beatae, amet possimus dolore accusantium officiis architecto, dignissimos nihil, expedita sapiente.
+                    Totam modi quo, fugiat at itaque quas praesentium dolor quasi velit obcaecati! Iusto odit non nihil aliquid porro consectetur dicta illum quae exercitationem nam, molestias incidunt delectus numquam laborum? Tempore.
+                </p>
+                <button className='bg-black text-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto md:mx-0 py-3'> Get Started</button>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default Delivery
