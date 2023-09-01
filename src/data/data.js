92926c2fbc7a2b45d08e001302bcc32324a4d927
rajat <rajat.k@sphinxworldbiz.co>
2023-09-01 15:58:07 +0530

    TopPicks components finished with datajs created and splide slider installed

---
+++ src/data/data.js
@@ -0,0 +1,150 @@
+export const topPicks = [
+    {
+        id: 1,
+        title: 'shawarma',
+        price: '$850',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549209/yum_imgs/Chicken-Shawarma-Wrap_2-SQ_acdrxb.png'
+    },
+    {
+        id: 2,
+        title: 'fruite juice',
+        price: '$900',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549210/yum_imgs/fruite_juice_ixgogp.png'
+    },
+    {
+        id: 3,
+        title: 'yogurt parfaite',
+        price: '$450',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549217/yum_imgs/yogurt-parfait-4_kmt5vr.png'
+    },
+    {
+        id: 4,
+        title: 'orange juice',
+        price: '$420',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549211/yum_imgs/orange-juice-splash_ulooub.png'
+    },
+    {
+        id: 5,
+        title: 'egg & plantain',
+        price: '$700',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549210/yum_imgs/egg_plantain_vaofmc.png'
+    },
+    {
+        id: 6,
+        title: 'tilapia fish',
+        price: '$900',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549210/yum_imgs/Fried-Tilapia_bb2kis.png'
+    },
+    {
+        id: 7,
+        title: 'chicken burger',
+        price: '$300',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549209/yum_imgs/chicken_butger_vohgpz.png'
+    },
+    {
+        id: 8,
+        title: 'egg & plantain',
+        price: '$500',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549210/yum_imgs/egg_plantain_vaofmc.png'
+    },
+    {
+        id: 9,
+        title: 'African Salad',
+        price: '$200',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549208/yum_imgs/African_Salad_oox0p5.png'
+    },
+    {
+        id: 10,
+        title: 'Jollof rice',
+        price: '$150',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549210/yum_imgs/Jollof_rice_zdbbct.png'
+    },
+    {
+        id: 11,
+        title: 'Chinese pizza',
+        price: '$900',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549209/yum_imgs/Chinese_pizza_mixj7x.png'
+    },
+];
+
+export const mealData = [
+    {
+        id: 5,
+        title: 'cheeseless pizza',
+        category: 'pizza',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549207/yum_imgs/Cheeseless-Pizza_et9ddo.png',
+        price: '$200',
+    },
+    {
+        id: 6,
+        title: 'Supreme pizza',
+        category: 'pizza',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549217/yum_imgs/Supreme_pizza_qrmmh5.png',
+        price: '$300',
+    },
+    {
+        id: 7,
+        title: 'Cheese pizza',
+        category: 'pizza',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549208/yum_imgs/Cheese_pizza_eys2ji.png',
+        price: '$140',
+    },
+    {
+        id: 8,
+        title: 'African Salad',
+        category: 'salad',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549208/yum_imgs/African_Salad_oox0p5.png',
+        price: '$300',
+    },
+    {
+        id: 9,
+        title: 'Ceaser Salad',
+        category: 'salad',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549208/yum_imgs/Ceaser_Salad_g2j26m.png',
+        price: '$300',
+    },
+    {
+        id: 10,
+        title: 'Loaded Salad',
+        category: 'salad',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549211/yum_imgs/Loaded_Salad_wgbbjl.png',
+        price: '$200',
+    },
+    {
+        id: 11,
+        title: 'Chicken Shawarma',
+        category: 'chicken',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549208/yum_imgs/Chicken_Shawarma_e82683.png',
+        price: '$350',
+    },
+    {
+        id: 12,
+        title: 'Chicken Tenders',
+        category: 'chicken',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549209/yum_imgs/Chicken_Tenders_k7seua.png',
+        price: '$100',
+    },
+
+];
+export const categories = [
+    {
+        id: 1,
+        title: 'Burger',
+        img: ''
+    },
+    {
+        id: 2,
+        title: 'veg',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549217/yum_imgs/veg_r5w4bv.png'
+    },
+    {
+        id: 1,
+        title: 'pizza',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549211/yum_imgs/pizza_vmulv4.png'
+    },
+    {
+        id: 2,
+        title: 'salad',
+        img: 'https://res.cloudinary.com/dcvhtfxa1/image/upload/v1693549216/yum_imgs/salad_nmwko5.png'
+    },
+];
